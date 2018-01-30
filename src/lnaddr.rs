570661ee6742a714d50a3effd2c6dac35f83acf4
Johann Og <kronolynx@gmail.com>
2018-01-30 17:59:07 +0100

    Add shorten amount function
    shorten a given amount in bitcoin

--- src/lnaddr.rs
+++ src/lnaddr.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+/// Given an amount in bitcoin, shorten it
+///
+/// BOLT #11:
+/// A writer MUST encode `amount` as a positive decimal integer with no
+/// leading zeroes, SHOULD use the shortest representation possible.
+pub fn shorten_amount(amount: f64) -> String {
+    let units = ["p", "n", "u", "m"];
+    // convert to pico initially
+    let pico_amount = (amount * (10f64).powi(12)) as u64;
+    shorten_amount_aux(pico_amount, &units)
+}
+
+fn shorten_amount_aux(amount: u64, units: &[&str]) -> String {
+    if units.len() == 0 {
+        amount.to_string()
+    } else if amount % 1000 == 0 {
+        shorten_amount_aux(amount / 1000, &units[1..])
+    } else {
+        amount.to_string() + units[0]
+    }
+}
+
+
 #[test]
 fn shorten_amount_test() {
     let mut test = HashMap::new();
