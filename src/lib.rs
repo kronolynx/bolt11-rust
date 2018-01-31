08a74811acc5491c88bfa71f0f2a9c0dd2bf9a9c
Johann Og <kronolynx@gmail.com>
2018-01-31 12:32:53 +0100

    Add bech32 dependency

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,15 @@
 #![deny(unused_mut)]
 #![warn(missing_docs)]
 
+/// Lightning Payments Example Implementation
+///
+/// This is an implementation of
+/// [Lightning BOLT11 Specification](https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md)
+/// for providing a minimal QR-code-ready format for requesting lightning payments.
+
+extern crate hex;
+extern crate bech32;
 mod lnaddr;
+
 
 
