9a3796fd096620c4a02c2cff83ba5808036dfbbe
Johann Og <kronolynx@gmail.com>
2018-02-05 11:36:05 +0100

    Add num dependency

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,12 @@
 /// This is an implementation of
 /// [Lightning BOLT11 Specification](https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md)
 /// for providing a minimal QR-code-ready format for requesting lightning payments.
-
+extern crate num;
 extern crate hex;
 extern crate bech32;
 mod lnaddr;
+
+
 
 
 
