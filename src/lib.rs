201faaa7f328a2812cb363b3bfe06a4d96f47692
Johann Og <kronolynx@gmail.com>
2018-02-08 16:08:05 +0100

    Add extern crate itertools

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,15 @@
 #![deny(unused_mut)]
 #![warn(missing_docs)]
 
+extern crate bech32;
+extern crate hex;
+extern crate itertools;
 /// Lightning Payments Example Implementation
 ///
 /// This is an implementation of
 /// [Lightning BOLT11 Specification](https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md)
 /// for providing a minimal QR-code-ready format for requesting lightning payments.
 extern crate num;
-extern crate hex;
-extern crate bech32;
 #[macro_use]
 mod macros;
 mod lnaddr;
-
-
-
-
-
