46a400d10a1fe0338498eae86e9874c6df07c741
Johann Og <kronolynx@gmail.com>
2018-02-15 15:50:30 +0100

    Implement encode decode timestamp

--- src/lnaddr.rs
+++ src/lnaddr.rs
@@ -1,9 +1,7 @@
 use std::num::ParseFloatError;
 use std::collections::HashMap;
 use utils::convert_bits;
-use types::ConvertResult;
-
-
+use types::{ConvertResult, U5};
 
 /// BOLT #11:
 /// The following **multiplier** letters are defined:
@@ -69,7 +67,7 @@ pub fn decode_amount(amount: &str) -> Result<f64, ParseFloatError> {
 }
 
 /// Convert a vector containing u5 values to u8
-fn bits_u5_to_u8(bytes: &Vec<u8>) -> ConvertResult {
+fn bits_u5_to_u8(bytes: &Vec<U5>) -> ConvertResult {
     convert_bits(bytes, 5, 8, false)
 }
 /// Convert a vector containing u8 values to u5
@@ -77,7 +75,26 @@ fn bits_u8_to_u5(bytes: &Vec<u8>) -> ConvertResult {
     convert_bits(bytes, 8, 5, false)
 }
 
+/// seconds-since-1970 (35 bits, big-endian)
+struct Timestamp;
 
+impl Timestamp {
+    /// decode timestamp from u5 vector
+    fn decode(data: &Vec<U5>) -> u64 {
+        data.iter().take(7).fold(0, |a, b| a * 32u64 + *b as u64)
+    }
+    /// encode timestamp
+    fn encode(timestamp: u64) -> Vec<U5> {
+        let mut acc: Vec<U5> = Vec::new();
+        let mut time_acc = timestamp;
+        while acc.len() < 7 {
+            acc.push((time_acc % 32) as U5);
+            time_acc /= 32;
+        }
+        acc.reverse();
+        acc
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -101,11 +118,25 @@ mod test {
     }
     #[test]
     fn u5_test() {
-        let u5_vec = vec![14, 20, 15, 7, 13, 26, 0, 25, 18, 6, 11, 13, 8, 21, 4, 20, 3, 17, 2, 29, 3, 12, 29, 3, 4, 15, 24, 20, 6, 14, 30, 22];
-        let u8_vec = vec![117, 30, 118, 232, 25, 145, 150, 212, 84, 148, 28, 69, 209, 179, 163, 35, 241, 67, 59, 214];
+        let u5_vec = vec![
+            14, 20, 15, 7, 13, 26, 0, 25, 18, 6, 11, 13, 8, 21, 4, 20, 3, 17, 2, 29, 3, 12, 29, 3,
+            4, 15, 24, 20, 6, 14, 30, 22,
+        ];
+        let u8_vec = vec![
+            117, 30, 118, 232, 25, 145, 150, 212, 84, 148, 28, 69, 209, 179, 163, 35, 241, 67, 59,
+            214,
+        ];
 
         assert!(bits_u5_to_u8(&u5_vec).unwrap().eq(&u8_vec));
         assert!(bits_u8_to_u5(&u8_vec).unwrap().eq(&u5_vec))
     }
-}
 
+    #[test]
+    fn timestamp_test() {
+        let data: Vec<U5> = vec![1, 12, 18, 31, 28, 25, 2];
+        let timestamp = 1496314658;
+
+        assert_eq!(Timestamp::decode(&data), timestamp);
+        assert!(data.eq(&Timestamp::encode(timestamp)));
+    }
+}
