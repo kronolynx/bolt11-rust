3d7ebfa0fb5f1923b80a0caf6901627bc807a593
Johann Og <kronolynx@gmail.com>
2018-03-07 18:16:50 +0100

    Implement fallback, hash, write

--- src/payment_request.rs
+++ src/payment_request.rs
@@ -1,13 +1,17 @@
-use bech32::Bech32;
+use bech32::{Bech32, create_checksum as bech32_checksum, CHARSET};
 use tag::Tag;
 use timestamp::Timestamp;
-use types::{Error, VecU5};
+use types::{Error, U5, VecU5};
 use secp256k1;
-use secp256k1::{Message, PublicKey, Signature};
+use secp256k1::{Message, PublicKey, SecretKey, Signature};
 use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 use amount::Amount;
 use std::fmt;
+use base58check::*;
+use itertools::Itertools;
+use bitcoin_bech32::WitnessProgram;
+use bitcoin_bech32::constants::Network;
 
 /// Lightning Payment Request
 /// * see https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md
@@ -25,6 +29,8 @@ pub struct PaymentRequest {
     pub tags: Vec<Tag>,
     /// request signature that will be checked against node id
     pub signature: Signature,
+    // recovery id
+    recovery_id: u8,
 }
 
 impl PaymentRequest {
@@ -40,7 +46,7 @@ impl PaymentRequest {
                 let recovery_id = secp256k1::RecoveryId::parse(bytes[len - 1])?;
                 bytes.remove(len - 1);
 
-                let signature = PaymentRequest::parse_signature(bytes.split_off(len - 65));
+                let signature = PaymentRequest::parse_signature(&bytes.split_off(len - 65));
 
                 let mut data = VecU5::from_u8_vec(&bytes)?;
 
@@ -63,6 +69,7 @@ impl PaymentRequest {
                         node_id,
                         tags,
                         signature,
+                        recovery_id: recovery_id.serialize(),
                     })
                 } else {
                     Err(Error::SignatureError(secp256k1::Error::InvalidSignature))
@@ -93,19 +100,127 @@ impl PaymentRequest {
             .next()
     }
 
+    /// the fallback address if any. It could be a script address, pubkey address, ..
+    pub fn fallback_address(&self) -> Option<String> {
+        // encode fallback address
+        fn bech_address(version: u8, program: Vec<u8>, network: Network) -> Option<String> {
+            let witness_program = WitnessProgram {
+                version,
+                program,
+                network,
+            };
+            witness_program.to_address().ok()
+        }
+        // prefix used in fallback closure
+        let prefix = &self.prefix;
+
+        let fallback = |tag: &Tag| -> Option<String> {
+            match *tag {
+                Tag::FallbackAddress {
+                    version: 17,
+                    ref hash,
+                } if prefix == "lnbc" =>
+                {
+                    // 0 PubkeyAddress
+                    Some(hash.to_base58check(0))
+                }
+                Tag::FallbackAddress {
+                    version: 18,
+                    ref hash,
+                } if prefix == "lnbc" =>
+                {
+                    // 5 ScriptAddress
+                    Some(hash.as_slice().to_base58check(5))
+                }
+                Tag::FallbackAddress {
+                    version: 17,
+                    ref hash,
+                } if prefix == "lntb" =>
+                {
+                    // 111 PubkeyAddressTestnet
+                    Some(hash.as_slice().to_base58check(111))
+                }
+                Tag::FallbackAddress {
+                    version: 18,
+                    ref hash,
+                } if prefix == "lntb" =>
+                {
+                    // 196 ScriptAddressTestnet
+                    Some(hash.as_slice().to_base58check(196))
+                }
+                Tag::FallbackAddress { version, ref hash } if prefix == "lnbc" => {
+                    bech_address(version, hash.to_owned(), Network::Bitcoin)
+                }
+                Tag::FallbackAddress { version, ref hash } if prefix == "lntb" => {
+                    bech_address(version, hash.to_owned(), Network::Testnet)
+                }
+                _ => None,
+            }
+        };
+
+        self.tags.iter().filter_map(fallback).next()
+    }
+
+    /// a representation of this payment request, without its signature, as a bit stream. This is what will be signed
+    pub fn stream(&self) -> Result<Vec<u8>, Error> {
+        let bytes = self.tags
+            .iter()
+            .flat_map(|tag| tag.to_vec_u5())
+            .collect_vec()
+            .concat();
+        let bytes = [Timestamp::encode(self.timestamp), bytes].concat();
+
+        VecU5::to_u8_vec(&bytes)
+    }
+
+    pub fn hash(&self) -> Result<Vec<u8>, Error> {
+        let amount = self.amount
+            .map_or(String::new(), |a| Amount::encode(a as f64));
+        let bytes = (self.prefix.to_owned() + &amount).as_bytes().to_vec();
+        self.stream()
+            .map(|s| PaymentRequest::sha256_hasher(&[bytes, s].concat()).to_vec())
+    }
+
+    pub fn write(&self) -> Result<String, Error> {
+        let hr_amount = self.amount
+            .map_or(String::new(), |a| Amount::encode(a as f64));
+        let mut hrp = self.prefix.to_owned() + &hr_amount;
+        let stream = [
+            self.stream()?,
+            self.signature.serialize().to_vec(),
+            vec![self.recovery_id],
+        ].concat();
+        let u5_stream = VecU5::from_u8_vec(&stream)?;
+        let checksum = bech32_checksum(&hrp.as_bytes().to_vec(), &u5_stream);
+
+        let stream_sum = [u5_stream, checksum]
+            .concat()
+            .iter()
+            .map(|i| CHARSET[*i as usize])
+            .collect::<String>();
+        hrp.push_str("1");
+        hrp.push_str(&stream_sum);
+        Ok(hrp)
+    }
+
     // parse the message
     fn parse_message(hrp: &String, bytes: &Vec<u8>) -> Message {
-        let mut raw_message = [0u8; 32];
         let message_bytes = [hrp.as_bytes(), bytes.as_slice()].concat();
-        let mut hasher = Sha256::new();
-        hasher.input(&message_bytes);
-        hasher.result(&mut raw_message);
+        let raw_message = PaymentRequest::sha256_hasher(&message_bytes);
 
         secp256k1::Message::parse(&raw_message)
     }
 
+    fn sha256_hasher(bytes: &Vec<u8>) -> [u8; 32] {
+        let mut hash = [0u8; 32];
+        let mut hasher = Sha256::new();
+        hasher.input(bytes);
+        hasher.result(&mut hash);
+        hash
+    }
+
     // parse the signature
-    fn parse_signature(bytes: Vec<u8>) -> Signature {
+    fn parse_signature(bytes: &Vec<u8>) -> Signature {
         let sig_raw = bytes
             .iter()
             .enumerate()
@@ -165,6 +280,16 @@ mod test {
          };
          static ref PUB_KEY: secp256k1::PublicKey = secp256k1::PublicKey::from_secret_key(&SEC_KEY);
     }
+    fn parse_public_key(bytes: &Vec<u8>) -> Result<PublicKey, Error> {
+        let pub_key_raw = bytes
+            .iter()
+            .enumerate()
+            .fold([0u8; 65], |mut acc, (index, item)| {
+                acc[index] = *item;
+                acc
+            });
+        secp256k1::PublicKey::parse(&pub_key_raw).map_err(Error::from)
+    }
 
     #[test]
     fn read() {
@@ -185,5 +310,8 @@ mod test {
             pay_request.description().unwrap().to_string(),
             "Please consider supporting this project".to_owned()
         );
+        assert_eq!(pay_request.fallback_address(), None);
+        assert_eq!(pay_request.tags.len(), 2);
+        assert_eq!(pay_request.write().unwrap(), tx_ref)
     }
 }
