8c23be5340cdf058cb8a6647b4d4371a9b6b631a
Johann Og <kronolynx@gmail.com>
2018-03-22 17:04:00 +0100

    Update modules visibility

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 #![deny(non_snake_case)]
 #![deny(unused_mut)]
 
-//! Lightning Payments Example Implementation
+//! Lightning Payments Implementation
 //!
 //! This is an implementation of
 //! [Lightning BOLT11 Specification](https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md)
@@ -25,8 +25,9 @@ extern crate secp256k1;
 mod macros;
 mod timestamp;
 mod utils;
-mod types;
-mod tag;
 mod amount;
 mod bech32;
-mod payment_request;
+
+pub mod types;
+pub mod tag;
+pub mod payment_request;
